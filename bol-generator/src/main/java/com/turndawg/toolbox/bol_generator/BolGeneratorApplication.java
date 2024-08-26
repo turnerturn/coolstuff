cdb7f2bde3c0f52cdfa22650cf487f258c6fc264
Matt Turner <matthewgeorgeturner@gmail.com>
2024-08-26 18:26:48 -0500

    fixings

---
+++ bol-generator/src/main/java/com/turndawg/toolbox/bol_generator/BolGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.turndawg.toolbox.bol_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolGeneratorApplication.class, args);
+	}
+
+}
---
+++ bol-generator/src/test/java/com/turndawg/toolbox/bol_generator/BolGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turndawg.toolbox.bol_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
